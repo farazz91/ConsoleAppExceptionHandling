486f74ec221ad1d984fa1ff3a193f66af43cbe75
farazz91 <farazz91@gmail.com>
2021-06-06 15:27:59 +0530

    Add project files.

---
+++ ConsoleAppExceptionHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleAppExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader smrd=null;
+            try
+            {
+              smrd = new StreamReader(@"D:/test.txt");
+                smrd.ReadToEnd();
+            }    
+            catch(FileNotFoundException fn)
+            {
+                Console.WriteLine("File {0} not found.",fn.FileName);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception occured {0}",ex.Message);
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                if(smrd != null)
+                {                    
+                    smrd.Close();
+                }
+            }
+        }
+    }
+}
